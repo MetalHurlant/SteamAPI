6c4f33ac722e5f75a6bf1c68cb09526c4cfcd869
Tom DUPUY <Tom DUPUY>
2017-03-09 16:42:15 +0100

    Info game

--- php/appinfo.php
+++ php/appinfo.php
@@ -20,6 +20,13 @@ if (empty($_SESSION['steam_uptodate']) or empty($_SESSION['steam_appid'])) {
 	if ($_SESSION['steam_gametype']=="game"){
 		$_SESSION['steam_dlc']=$content[$appid]['data']['dlc'];
 	};
+	$_SESSION['steam_detailed_description'] = $content[$appid]['data']['detailed_description'];
+	$_SESSION['steam_supported_languages'] = $content[$appid]['data']['supported_languages'];
+	$_SESSION['header_image'] = $content[$appid]['data']['header_image'];
+	$_SESSION['currency'] = $content[$appid]['data']['price_overview']['currency'];
+	$_SESSION['initialPrice'] = $content[$appid]['data']['price_overview']['initial'];
+	$_SESSION['finalPrice'] = $content[$appid]['data']['price_overview']['final'];
+	$_SESSION['releaseDate'] = $content[$appid]['data']['release_date']['date'];
 	
 	$_SESSION['steam_uptodate'] = time();
 }
@@ -41,8 +48,14 @@ if ($_SESSION['steam_isfree']) {
 };
 if ($_SESSION['steam_gametype']=="game") {
 	$tpl->assign('dlcs',$_SESSION['steam_dlc']);
-}
-
+};
+$tpl->assign('description',$_SESSION['steam_detailed_description']);
+$tpl->assign('languages',$_SESSION['steam_supported_languages']);
+$tpl->assign('headerImage',$_SESSION['header_image']);
+$tpl->assign('currency',$_SESSION['currency']);
+$tpl->assign('initialPrice',$_SESSION['initialPrice']/100);
+$tpl->assign('finalPrice',$_SESSION['finalPrice']/100);
+$tpl->assign('releaseDate',$_SESSION['releaseDate']);
 
 $tpl->assign('apikey','D004601E2AC5480D014CBD90EE981BA4');
 $tpl->assign('json',$_SESSION['jsonurl']);
